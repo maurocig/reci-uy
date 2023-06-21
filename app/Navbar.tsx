1fc28558960361c587817b81278b9f323f873105
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-06-21 17:18:22 -0300

    layout min height ok

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -15,9 +15,12 @@ const links = [
 
 export default function Navbar({}: NavbarProps) {
    return (
-      <div className="flex items-center justify-between h-16 px-4 bg-slate-200">
-         <div className="relative h-10 p-4 overflow-hidden w-[200px]">
-            <Image src="/images/logo.png" alt="Logo de Reci" className="object-contain" fill />
+      <div className="flex items-center justify-between h-20 px-4 bg-slate-200">
+         <div className="">
+            <div className="relative h-[35px] overflow-hidden w-[200px]">
+               <Image src="/images/logo.png" alt="Logotipo de Reci" className="object-contain" fill />
+            </div>
+            <p className="m-0 text-[.65rem] font-semibold text-center ml-[80px] absolute">IMPORTACIONES</p>
          </div>
       </div>
    );
