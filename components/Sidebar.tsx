7700aefe612fa4ee2fc84778e9dd88a9055e5078
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-08-24 12:13:10 -0300

    fixed position sidebar!

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { usePathname } from "next/navigation";
-import { FaRegSnowflake, FaTruckLoading, FaWrench } from "react-icons/fa";
-import { HiHome, HiInformationCircle, HiMail } from "react-icons/hi";
-import { IoIosSnow, IoMdSnow } from "react-icons/io";
-import { MdInfo } from "react-icons/md";
-import { RiTruckFill } from "react-icons/ri";
-import { TbSnowflake } from "react-icons/tb";
-import { twMerge } from "tailwind-merge";
-import Box from "./Box";
-import SidebarItem from "./SidebarItem";
-import Spacer from "./ui/Spacer";
+import { usePathname } from 'next/navigation';
+import { FaRegSnowflake, FaTruckLoading, FaWrench } from 'react-icons/fa';
+import { HiHome, HiInformationCircle, HiMail } from 'react-icons/hi';
+import { IoIosSnow, IoMdSnow } from 'react-icons/io';
+import { MdInfo } from 'react-icons/md';
+import { RiTruckFill } from 'react-icons/ri';
+import { TbSnowflake } from 'react-icons/tb';
+import { twMerge } from 'tailwind-merge';
+import Box from './Box';
+import SidebarItem from './SidebarItem';
+import Spacer from './ui/Spacer';
 
 type SidebarProps = {
   className?: string;
@@ -21,95 +21,97 @@ export default function Sidebar({ className }: SidebarProps) {
 
   const mainItems = [
     {
-      name: "Inicio",
-      url: "/",
+      name: 'Inicio',
+      url: '/',
       icon: HiHome,
-      active: pathname === "/",
+      active: pathname === '/',
     },
   ];
 
   const productItems = [
     {
-      name: "Equipos de frío",
-      url: "/equipos",
+      name: 'Equipos de frío',
+      url: '/equipos',
       icon: TbSnowflake,
-      active: pathname === "/equipos",
+      active: pathname === '/equipos',
     },
     {
-      name: "Carrocerías",
-      url: "/carrocerias",
+      name: 'Carrocerías',
+      url: '/carrocerias',
       icon: RiTruckFill,
-      active: pathname === "/carrocerias",
+      active: pathname === '/carrocerias',
     },
     {
-      name: "Accesorios",
-      url: "/accesorios",
+      name: 'Accesorios',
+      url: '/accesorios',
       icon: FaTruckLoading,
-      active: pathname === "/accesorios",
+      active: pathname === '/accesorios',
     },
   ];
 
   const otherItems = [
     {
-      name: "Servicio técnico",
-      url: "/servicios",
+      name: 'Servicio técnico',
+      url: '/servicios',
       icon: FaWrench,
-      active: pathname === "/servicios",
+      active: pathname === '/servicios',
     },
     {
-      name: "Nosotros",
-      url: "/nosotros",
+      name: 'Nosotros',
+      url: '/nosotros',
       icon: MdInfo,
-      active: pathname === "/nosotros",
+      active: pathname === '/nosotros',
     },
     {
-      name: "Contacto",
-      url: "/contacto",
+      name: 'Contacto',
+      url: '/contacto',
       icon: HiMail,
-      active: pathname === "/contacto",
+      active: pathname === '/contacto',
     },
   ];
 
   return (
-    <div className={twMerge("min-w-[230px] ", className)}>
-      {/* INICIO */}
-      <Box>
-        {mainItems.map((item, i) => (
-          <SidebarItem
-            name={item.name}
-            url={item.url}
-            icon={item.icon}
-            active={item.active}
-            key={"item" + i}
-          />
-        ))}
-      </Box>
-      <Spacer className="h-4" />
-      {/* Productos */}
-      <Box>
-        {productItems.map((item, i) => (
-          <SidebarItem
-            active={item.active}
-            name={item.name}
-            url={item.url}
-            icon={item.icon}
-            key={"productItem" + i}
-          />
-        ))}
-      </Box>
-      <Spacer className="h-4" />
-      {/* Otros */}
-      <Box>
-        {otherItems.map((item, i) => (
-          <SidebarItem
-            active={item.active}
-            name={item.name}
-            url={item.url}
-            icon={item.icon}
-            key={"productItem" + i}
-          />
-        ))}
-      </Box>
+    <div className={twMerge('min-w-[230px] ', className)}>
+      <div className="fixed min-w-[230px]">
+        {/* INICIO */}
+        <Box>
+          {mainItems.map((item, i) => (
+            <SidebarItem
+              name={item.name}
+              url={item.url}
+              icon={item.icon}
+              active={item.active}
+              key={'item' + i}
+            />
+          ))}
+        </Box>
+        <Spacer className="h-4" />
+        {/* Productos */}
+        <Box>
+          {productItems.map((item, i) => (
+            <SidebarItem
+              active={item.active}
+              name={item.name}
+              url={item.url}
+              icon={item.icon}
+              key={'productItem' + i}
+            />
+          ))}
+        </Box>
+        <Spacer className="h-4" />
+        {/* Otros */}
+        <Box>
+          {otherItems.map((item, i) => (
+            <SidebarItem
+              active={item.active}
+              name={item.name}
+              url={item.url}
+              icon={item.icon}
+              key={'productItem' + i}
+            />
+          ))}
+        </Box>
+      </div>
     </div>
   );
 }
--- components/modals/mobile-menu-sheet.tsx
+++ components/modals/mobile-menu-sheet.tsx
@@ -1,8 +1,8 @@
-"use client";
-import Sidebar from "@/components/Sidebar";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+'use client';
+import Sidebar from '@/components/Sidebar';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import {
   Sheet,
   SheetClose,
@@ -12,15 +12,15 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet";
-import { useMobileMenu } from "@/hooks/useMobileMenu";
+} from '@/components/ui/sheet';
+import { useMobileMenu } from '@/hooks/useMobileMenu';
 
 export function MobileMenuSheet() {
   const mobileMenu = useMobileMenu();
 
   return (
     <Sheet open={mobileMenu.isOpen} onOpenChange={mobileMenu.onClose}>
-      <SheetContent className="border-none shadow-lg bg-slate-800/95">
+      <SheetContent className="transition border-none shadow-lg w-fit bg-gradient-to-br from-slate-900/90 via-slate-800/80 to-teal-900/90">
         <SheetHeader>
           <SheetTitle className="text-white">Menú</SheetTitle>
         </SheetHeader>
