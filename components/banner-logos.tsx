ed2cbc70669346e15ed03e8797e7b60fe7e8ded7
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-08-31 20:07:41 -0300

    add

---
+++ components/banner-logos.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+
+type BannerLogosProps = {
+  logosUrl: string[];
+  logosHorUrl: string[];
+};
+
+export default function BannerLogos({
+  logosUrl,
+  logosHorUrl,
+}: BannerLogosProps) {
+  return (
+    <div className="flex justify-around w-full h-[70px]">
+      {logosUrl.map((url) => (
+        <div
+          className="w-[120px] aspect-video relative h-full sm:hidden"
+          key={url}
+        >
+          <Image
+            src={url}
+            alt="logotipo"
+            fill
+            className="relative object-cover h-[50px] drop-shadow-darker"
+          />
+        </div>
+      ))}
+      {logosHorUrl.map((url) => (
+        <div className="relative hidden w-full sm:block" key={url}>
+          <Image
+            src={url}
+            alt="logotipo"
+            fill
+            className="relative object-contain drop-shadow-darker"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
