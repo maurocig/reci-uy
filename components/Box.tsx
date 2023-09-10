62a0f6804090bf3f9510cc5fcd62c9698db77e21
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-09-10 00:35:16 -0300

    removed bg blur and gradient from box component to better performance

--- components/Box.tsx
+++ components/Box.tsx
@@ -9,7 +9,8 @@ export default function Box({ children, className }: BoxProps) {
   return (
     <div
       className={twMerge(
-        `flex flex-col ring-gray-900/20 ring-[1px] rounded-lg bg-gradient-to-br from-slate-800/40 to-slate-800/20 p-3 md:p-4 shadow-lg backdrop-blur-md `,
+        // `flex flex-col ring-gray-900/20 ring-[1px] rounded-lg bg-gradient-to-br from-slate-800/40 to-slate-800/20 p-3 md:p-4 shadow-lg backdrop-blur-md `,
+        `flex flex-col ring-gray-900/20 ring-[1px] rounded-lg bg-slate-800/30 p-3 md:p-4 shadow-lg  `,
         className
       )}
     >
