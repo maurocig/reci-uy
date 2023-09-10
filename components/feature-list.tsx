b46f94fc5ac573559f14d0702c9cd672c7b04f0d
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-09-10 01:54:17 -0300

    feature list icon style updated

--- components/feature-list.tsx
+++ components/feature-list.tsx
@@ -11,7 +11,11 @@ export default function FeatureList({ features }: FeatureListProps) {
         {features.map((feature) => (
           <li key={feature} className="flex items-center gap-2">
             <div className="flex items-start content-start w-6">
-              <Check className="inline" size={16} />
+              <Check
+                className="inline text-emerald-400"
+                strokeWidth={4}
+                size={19}
+              />
             </div>
             <div className="w-full">{feature}</div>
           </li>
