8630b127c00bac759cbabb51d9dacd192714117d
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-09-25 16:57:50 -0300

    form working with resend

--- app/api/contact/route.tsx
+++ app/api/contact/route.tsx
@@ -1,7 +1,9 @@
 import { Email } from '@/components/email';
+import EmailTemplate from '@/components/email-template';
 import { smtpEmail, targetEmail, transporter } from '@/lib/nodemailer';
 import { render } from '@react-email/components';
 import { NextRequest, NextResponse } from 'next/server';
+import { Resend } from 'resend';
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const body = await req.json();
@@ -11,16 +13,30 @@ export async function POST(req: NextRequest, res: NextResponse) {
     <Email name={name} email={email} message={message} />
   );
 
-  const options = {
-    from: smtpEmail,
-    to: targetEmail,
-    subject: 'Nuevo mensaje de formulario (reci.uy)',
-    html: emailHtml,
-  };
+  // con node mailer
+  /* const options = { */
+  /*   from: smtpEmail, */
+  /*   to: targetEmail, */
+  /*   subject: 'Nuevo mensaje de formulario (reci.uy)', */
+  /*   html: emailHtml, */
+  /* }; */
 
   try {
-    transporter.sendMail(options);
-    // console.log(name, email, message);
+    // con node mailer
+    // transporter.sendMail(options);
+
+    console.log('llego a route.tsx', name, email, message);
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: 'mcigliuti01@gmail.com',
+      subject: 'Nuevo mensaje de formulario (reci.uy)',
+      html: emailHtml,
+    });
+
+    return NextResponse.json(data);
   } catch (error) {
     console.error('Hubo un error al enviar el email', error);
   }
