53697109926297387f06e44c9c76fd3799fc0c2c
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-11-29 12:36:32 -0300

    changed path from carrocerias page to box

--- app/equipos/page.tsx
+++ app/equipos/page.tsx
@@ -1,5 +1,5 @@
 // local import
-import Box from '@/components/box.jsx';
+import Box from '@/components/box';
 import ItemList, { ProductItem } from '@/components/ItemList';
 import BannerCategory from '@/components/banner-category';
 import { Product } from '@/types.js';
