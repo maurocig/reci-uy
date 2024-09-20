1c203f82bec0e4880b6ef4adf8081c75b7241b21
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-09-20 16:47:56 -0300

    vx series banner with v-600x

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Box from '@/components/box';
-import Banner from '@/components/banner';
-import BannerLogos from '@/components/banner-logos';
-import FeatureList from '@/components/feature-list';
-import HomeIconsSection from '@/components/home-icons-section';
+import Banner from "@/components/banner";
+import BannerLogos from "@/components/banner-logos";
+import Box from "@/components/box";
+import FeatureList from "@/components/feature-list";
+import HomeIconsSection from "@/components/home-icons-section";
+import ItemList from "@/components/ItemList";
+import { equipos } from "@/public/assets/equipos";
+import { Product } from "@/types";
 import {
   ArrowRightCircle,
   CalendarCheck,
@@ -14,61 +17,59 @@ import {
   Newspaper,
   Square,
   Wrench,
-} from 'lucide-react';
-import Image from 'next/image';
-import { FaList, FaTruckLoading, FaWrench } from 'react-icons/fa';
-import { IoIosPeople, IoMdPeople } from 'react-icons/io';
-import { RiTruckFill } from 'react-icons/ri';
-import { TbSnowflake } from 'react-icons/tb';
-import { Balancer } from 'react-wrap-balancer';
-import ItemList from '@/components/ItemList';
-import { equipos } from '@/public/assets/equipos';
-import { Product } from '@/types';
+} from "lucide-react";
+import Image from "next/image";
+import { FaList, FaTruckLoading, FaWrench } from "react-icons/fa";
+import { IoIosPeople, IoMdPeople } from "react-icons/io";
+import { RiTruckFill } from "react-icons/ri";
+import { TbSnowflake } from "react-icons/tb";
+import { Balancer } from "react-wrap-balancer";
 
 export default function Home() {
   const productItems = [
     {
       icon: TbSnowflake,
-      text: 'Equipos de frío',
-      url: '/equipos',
-      imageUrl: '/images/equipo-trailer.jpg',
+      text: "Equipos de frío",
+      url: "/equipos",
+      imageUrl: "/images/equipo-trailer.jpg",
     },
     {
       icon: RiTruckFill,
-      text: 'Carrocerías',
-      url: '/carrocerias',
-      imageUrl: '/images/liderkit-atras.jpg',
+      text: "Carrocerías",
+      url: "/carrocerias",
+      imageUrl: "/images/liderkit-atras.jpg",
     },
     {
       icon: FaTruckLoading,
-      text: 'Accesorios',
-      url: '/accesorios',
-      imageUrl: '/images/rampa-palfinger.jpg',
+      text: "Accesorios",
+      url: "/accesorios",
+      imageUrl: "/images/rampa-palfinger.jpg",
     },
   ];
 
   const serviceItems = [
     {
       icon: FaWrench,
-      text: 'Servicio técnico',
-      url: '/servicios#servicio-tecnico',
-      imageUrl: '',
+      text: "Servicio técnico",
+      url: "/servicios#servicio-tecnico",
+      imageUrl: "",
     },
     {
       icon: IoIosPeople,
-      text: 'Asesoramiento',
-      url: '/servicios#asesoramiento',
-      imageUrl: '',
+      text: "Asesoramiento",
+      url: "/servicios#asesoramiento",
+      imageUrl: "",
     },
     {
       icon: FaTruckLoading,
-      text: 'Accesorios',
-      url: '/accesorios',
-      imageUrl: '',
+      text: "Accesorios",
+      url: "/accesorios",
+      imageUrl: "",
     },
   ];
 
-  const ev: Product[] = [equipos[equipos.length - 1]];
+  const ev: Product[] = [equipos[equipos.length - 2]];
+  const vx: Product[] = [equipos[equipos.length - 1]];
 
   return (
     <div className="flex flex-col space-y-4 lg:space-y-6">
@@ -79,16 +80,51 @@ export default function Home() {
         image="/images/edit/portada-a-2.png"
       />
 
-      {/* EV SERIES */}
-      <Box className="lg:p-6">
+      <Box className="space-y-4 lg:space-y-6 lg:p-6">
+        <h2 className="flex items-center gap-2 mb-0 text-2xl font-semibold">
+          <Newspaper size={19} /> Novedades
+        </h2>
+
+        {/* VX Series */}
+        <div className="grid gap-4 lg:grid-cols-3 lg:gap-6 ">
+          <Box className="hidden w-full col-span-2 border border-slate-200/10 bg-sky-400/10 lg:block ">
+            <h2 className="flex items-center w-full gap-2 mb-2 text-2xl font-semibold text-center sm:text-start">
+              Nueva Serie VX
+            </h2>
+            <p className="mb-2 text-lg text-slate-300">
+              Sin motor, de alta eficiencia y con un bajo nivel de ruido.
+            </p>
+            <p className="hidden xl:block">
+              La serie VX de Thermo King eleva aún más el nivel de las
+              soluciones de refrigeración monotemperatura y multitemperatura
+              para camiones de pequeño, medio y gran tamaño al proporcionar una
+              refrigeración sin emisiones, con un nivel extremadamente bajo de
+              ruido y vibraciones, un mantenimiento reducido, un bajo coste
+              total de propiedad y mucho más.
+            </p>
+            <a
+              href="/equipos/ev-500"
+              className="flex items-center justify-start w-full gap-2 mt-4 group text-cyan-400 opacity-80 hover:opacity-100"
+            >
+              <span className="group-hover:underline">Ver producto</span>{" "}
+              <ArrowRightCircle size={15} />
+            </a>
+          </Box>
+          <ItemList
+            gridCols="sm:grid-cols-1 lg:grid-cols-1"
+            // description="Unidades 100% eléctricas para camiones medianos y grandes"
+            products={vx}
+            buttonText="Ver producto"
+          />
+        </div>
+
+        {/* EV SERIES */}
         <ItemList
-          title="Novedades"
-          icon
           // description="Unidades 100% eléctricas para camiones medianos y grandes"
           products={ev}
           buttonText="Ver producto"
         >
-          <Box className="hidden w-full col-span-2 border border-slate-200/10 lg:block bg-sky-400/10 ">
+          <Box className="hidden w-full col-span-2 border border-slate-200/10 bg-sky-400/10 lg:block ">
             <h2 className="flex items-center w-full gap-2 mb-2 text-2xl font-semibold text-center sm:text-start">
               Thermo King Serie EV para vehículos eléctricos
             </h2>
@@ -103,9 +139,9 @@ export default function Home() {
             </p>
             <a
               href="/equipos/ev-500"
-              className="flex items-center justify-start w-full gap-2 mt-4 opacity-80 hover:opacity-100 group text-cyan-400"
+              className="flex items-center justify-start w-full gap-2 mt-4 group text-cyan-400 opacity-80 hover:opacity-100"
             >
-              <span className="group-hover:underline">Ver producto</span>{' '}
+              <span className="group-hover:underline">Ver producto</span>{" "}
               <ArrowRightCircle size={15} />
             </a>
           </Box>
@@ -119,26 +155,26 @@ export default function Home() {
 
       {/* Servicio técnico */}
       <Box className="">
-        <div className="grid w-full gap-6 lg:gap-6 h-fit sm:grid-cols-2 ">
+        <div className="grid w-full gap-6 h-fit sm:grid-cols-2 lg:gap-6 ">
           <div className="bg-opacity-0 rounded-lg border-cyan-100/80 ">
             <h2 className="flex items-center w-full gap-2 mt-0 text-2xl font-semibold text-center sm:text-start">
               <Wrench className="inline" size={19} /> Servicio técnico
             </h2>
-            <p className="mb-2 xl:text-lg text-gray-400 ms-[25px]">
+            <p className="mb-2 ms-[25px] text-gray-400 xl:text-lg">
               Realizado por técnicos especializados Thermo King.
             </p>
             <FeatureList
               features={[
-                'Personal especializado',
-                'Repuestos originales',
-                'Venta y colocación',
-                'Service y mantenimiento',
+                "Personal especializado",
+                "Repuestos originales",
+                "Venta y colocación",
+                "Service y mantenimiento",
               ]}
             />
           </div>
           <div className="relative col-span-1">
             <Image
-              src={'/images/servicio-tk.jpg'}
+              src={"/images/servicio-tk.jpg"}
               alt="Servicio técnico"
               fill
               className="object-cover object-center rounded-lg shadow-lg"
@@ -153,7 +189,7 @@ export default function Home() {
           // style={{ backgroundImage: 'url(/images/asesoramiento-2.jpg' }}
           className="bg-center bg-cover rounded-lg bg-opacity-5 "
         >
-          <Box className="h-full lg:p-6 backdrop-blur">
+          <Box className="h-full backdrop-blur lg:p-6">
             <h2 className="flex items-center w-full gap-2 mb-2 text-2xl font-semibold text-center sm:text-start">
               <FileText className="inline" size={19} /> Asesoramiento
             </h2>
@@ -170,9 +206,9 @@ export default function Home() {
               </p>
               <a
                 href="/contacto"
-                className="flex items-center justify-start w-full gap-2 opacity-80 hover:opacity-100 group text-cyan-400"
+                className="flex items-center justify-start w-full gap-2 group text-cyan-400 opacity-80 hover:opacity-100"
               >
-                <span className="group-hover:underline">Quiero asesorarme</span>{' '}
+                <span className="group-hover:underline">Quiero asesorarme</span>{" "}
                 <ArrowRightCircle size={15} />
               </a>
             </div>
@@ -182,7 +218,7 @@ export default function Home() {
           // style={{ backgroundImage: 'url(/images/asesoramiento-stock.jpeg' }}
           className="bg-center bg-cover rounded-lg bg-opacity-5 "
         >
-          <Box className="h-full lg:p-6 backdrop-blur">
+          <Box className="h-full backdrop-blur lg:p-6">
             <h2 className="flex items-center w-full gap-2 mb-2 text-2xl font-semibold text-center sm:text-start">
               <CalendarCheck2 className="inline" size={18} /> Servicio post
               venta
@@ -210,14 +246,14 @@ export default function Home() {
       <div className="p-4 rounded-lg shadow-lg bg-white/20 lg:p-6">
         <BannerLogos
           logosUrl={[
-            '/images/logos/logo-lk.png',
-            '/images/logos/logo-fb.png',
-            '/images/logos/logo-tk-full.png',
+            "/images/logos/logo-lk.png",
+            "/images/logos/logo-fb.png",
+            "/images/logos/logo-tk-full.png",
           ]}
           logosHorUrl={[
-            '/images/logos/logo-tk-hor.png',
-            '/images/logos/logo-fb.png',
-            '/images/logos/logo-lk-hor-2.png',
+            "/images/logos/logo-tk-hor.png",
+            "/images/logos/logo-fb.png",
+            "/images/logos/logo-lk-hor-2.png",
           ]}
         />
       </div>
--- components/ItemList.tsx
+++ components/ItemList.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { cn } from '@/lib/utils';
-import { Product } from '@/types';
-import Link from 'next/link';
-import { IconType } from 'react-icons';
-import Item from './Item';
-import Box from '@/components/box';
-import { LucideIcon, Newspaper } from 'lucide-react';
+import Box from "@/components/box";
+import { cn } from "@/lib/utils";
+import { Product } from "@/types";
+import { LucideIcon, Newspaper } from "lucide-react";
+import Link from "next/link";
+import { IconType } from "react-icons";
+import Item from "./Item";
 
 export interface ProductItem extends Product {
   twClass?: string;
@@ -39,30 +39,34 @@ export default function ItemList({
 }: ItemListProps) {
   products.forEach((product) => {
     switch (product.brand) {
-      case 'Liderkit':
-        product.twClass = 'bg-orange-400';
+      case "Liderkit":
+        product.twClass = "bg-orange-400";
         return;
-      case 'Thermo King':
-        product.twClass = 'bg-sky-300';
+      case "Thermo King":
+        product.twClass = "bg-sky-300";
         return;
-      case 'Thermo King Line':
-        product.twClass = 'bg-sky-300 text-lg';
+      case "Thermo King Line":
+        product.twClass = "bg-sky-300 text-lg";
         return;
-      case 'Fibrasil':
-        product.twClass = 'bg-white';
+      case "Fibrasil":
+        product.twClass = "bg-white";
         return;
     }
   });
 
   return (
     <div>
-      <h2 className="mb-0 flex items-center gap-2 text-2xl font-semibold">
-        {Icon && <Newspaper size={19} />} {title}
-      </h2>
-      <p className="mb-4 text-lg text-slate-400">{description}</p>
+      {title && (
+        <>
+          <h2 className="flex items-center gap-2 mb-0 text-2xl font-semibold">
+            {Icon && <Newspaper size={19} />} {title}
+          </h2>
+          <p className="mb-4 text-lg text-slate-400">{description}</p>
+        </>
+      )}
       <div
         className={cn(
-          'grid grid-cols-1 gap-4 sm:gap-4 xl:gap-6 sm:grid-cols-2 lg:grid-cols-3 ',
+          "grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 xl:gap-6 ",
           gridCols
         )}
       >
@@ -71,12 +75,12 @@ export default function ItemList({
           return (
             <div key={`${product.model}${i}`}>
               <div>
-                <Link href={product.url || ''} target={newTab ? '_blank' : ''}>
+                <Link href={product.url || ""} target={newTab ? "_blank" : ""}>
                   <Item
                     product={product}
                     showDescription={showDescription}
                     tagColor={product.twClass}
-                    buttonText={buttonText || 'Ver producto'}
+                    buttonText={buttonText || "Ver producto"}
                     buttonIcon={buttonIcon}
                   />
                 </Link>
