1b1deaecc5e6a18cff54f0e9fc84ceee1b2f0260
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-09-26 11:56:02 -0300

    añadida linea vx a seccion /equipos

--- app/equipos/page.tsx
+++ app/equipos/page.tsx
@@ -1,22 +1,24 @@
 // local import
-import Box from '@/components/box';
-import ItemList, { ProductItem } from '@/components/ItemList';
-import BannerCategory from '@/components/banner-category';
-import { Product } from '@/types.js';
-import { equipos } from '../../public/assets/equipos.js';
-import { ArrowRightCircle } from 'lucide-react';
+import BannerCategory from "@/components/banner-category";
+import Box from "@/components/box";
+import ItemList, { ProductItem } from "@/components/ItemList";
+import { Product } from "@/types.js";
+import { ArrowRightCircle } from "lucide-react";
+import { equipos } from "../../public/assets/equipos.js";
 
 export default function EquiposPage() {
   const vp: Product[] = [];
   const ev: Product[] = [];
   const truck: Product[] = [];
   const trailer: Product[] = [];
+  const vx: Product[] = [];
 
   equipos.forEach((equipo) => {
-    equipo.line === 'EV' && ev.push(equipo);
-    equipo.line === 'VP Truck' && vp.push(equipo);
-    equipo.line === 'Truck' && truck.push(equipo);
-    equipo.line === 'Trailer' && trailer.push(equipo);
+    equipo.line === "EV" && ev.push(equipo);
+    equipo.line === "VP Truck" && vp.push(equipo);
+    equipo.line === "Truck" && truck.push(equipo);
+    equipo.line === "Trailer" && trailer.push(equipo);
+    equipo.line === "VX" && vx.push(equipo);
   });
 
   return (
@@ -25,7 +27,7 @@ export default function EquiposPage() {
         image="/images/equipo-trailer.jpg"
         title="Equipos de frío"
         description="Línea Thermo King."
-        className="bg-gradient-to-r shadow-lg from-slate-200 via-slate-200/80 to-slate-200/30 backdrop-blur-[1px] md:via-slate-300/20 md:to-transparent md:backdrop-blur-0 "
+        className="bg-gradient-to-r from-slate-200 via-slate-200/80 to-slate-200/30 shadow-lg backdrop-blur-[1px] md:via-slate-300/20 md:to-transparent md:backdrop-blur-0 "
         bgPosition="sm:bg-[right_top_-5rem] md:bg-[right_top_-10rem] lg:bg-[right_top_-10rem] xl:bg-[right_top_-20rem]"
       />
 
@@ -35,30 +37,25 @@ export default function EquiposPage() {
           description="Unidades 100% eléctricas para camiones medianos y grandes"
           products={ev}
           buttonText="Ver producto"
-        >
-          <Box className="hidden w-full col-span-2 border border-slate-200/10 lg:block bg-sky-400/10 ">
-            <h2 className="flex items-center w-full gap-2 mb-2 text-2xl font-semibold text-center sm:text-start">
-              Thermo King Serie EV para vehículos eléctricos
-            </h2>
-            <p className="mb-2 text-lg text-slate-300">
-              Instalamos el primer equipo Thermo King 100% eléctrico de
-              latinoamérica.
-            </p>
-            <p className="hidden xl:block">
-              La alta fiabilidad, eficiencia energética y rendimiento de Thermo
-              King llegan al transporte eléctrico. Ideal para transporte de
-              alimentos y productos farmacéuticos en la cadena de frío.
-            </p>
+        ></ItemList>
+      </Box>
 
-            <a
-              href="/equipos/ev-500"
-              className="flex items-center justify-start w-full gap-2 mt-4 opacity-80 hover:opacity-100 group text-cyan-400"
-            >
-              <span className="group-hover:underline">Ver producto</span>{' '}
-              <ArrowRightCircle size={15} />
-            </a>
-          </Box>
-        </ItemList>
+      {/* VX Series */}
+      <Box className="overflow-hidden lg:p-6">
+        {/* New tag */}
+        {/* <div
+          className={` mr-[-50px] flex rotate-45 items-center self-end rounded-none bg-red-500 px-10 py-1 text-start font-semibold text-gray-900 shadow-sm ring-black transition lg:mr-[-60px]`}
+        >
+          <span className="flex items-center justify-end ml-2 text-white">
+            Nuevos!
+          </span>
+        </div> */}
+        <ItemList
+          title="Serie VX"
+          description="Refrigeración silenciosa para camiones pequeños, medianos y grandes"
+          products={vx}
+          buttonText="Ver producto"
+        />
       </Box>
 
       <Box className="lg:p-6">
